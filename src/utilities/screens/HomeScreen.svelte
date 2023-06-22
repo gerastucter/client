6c34c929f3abee4c5d32526d857d64f2ecfb9df3
* <gerastucter@gmail.com>
2023-06-21 21:41:39 -0300

    V1.1.5

--- src/utilities/screens/HomeScreen.svelte
+++ src/utilities/screens/HomeScreen.svelte
@@ -12,6 +12,7 @@
 
 
 <main class="container mx-auto text-center p-4 md:text-center mb-5">
+  
   <section class="relative flex flex-col items-center justify-center">
     <div class="relative z-[1] space-y-6 pt-32 pb-24">
       
@@ -39,12 +40,12 @@
         </div>
       </div>
       <div class="z-40">
-        <h1 class="font-bold text-4xl tracking-tight text-center mb-4">
+        <h1 class="font-bold text-4xl tracking-tight text-center mb-4 p-5">
           <span class="mb-2">Welcome to</span>
           <span class="
           text-success-50-900-token bg-tertiary-900-50-token p-1 rounded drop-shadow-lg">Gerastucter</span>
         </h1>
-        <p class="max-w-lg opacity-70 md:mx-auto text-center">
+        <p class="max-w-lg opacity-70 md:mx-auto text-center pattern-cross-dots-sm p-6">
           Get to know tools that will help you a lot in your language learning, here you have the freedom to browse
           all
           content without any limitation. Discover our resources or know more about us...
@@ -60,10 +61,31 @@
             <InfoIcon class="p-1" size="24" />
           </a>
         </div>
+        <blockquote class="relative p-4">
+          <svg
+            class="absolute top-0 left-0 transform -translate-x-6 -translate-y-8 h-16 w-16 text-gray-100 dark:text-gray-700"
+            width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true">
+            <path
+              d="M7.39762 10.3C7.39762 11.0733 7.14888 11.7 6.6514 12.18C6.15392 12.6333 5.52552 12.86 4.76621 12.86C3.84979 12.86 3.09047 12.5533 2.48825 11.94C1.91222 11.3266 1.62421 10.4467 1.62421 9.29999C1.62421 8.07332 1.96459 6.87332 2.64535 5.69999C3.35231 4.49999 4.33418 3.55332 5.59098 2.85999L6.4943 4.25999C5.81354 4.73999 5.26369 5.27332 4.84476 5.85999C4.45201 6.44666 4.19017 7.12666 4.05926 7.89999C4.29491 7.79332 4.56983 7.73999 4.88403 7.73999C5.61716 7.73999 6.21938 7.97999 6.69067 8.45999C7.16197 8.93999 7.39762 9.55333 7.39762 10.3ZM14.6242 10.3C14.6242 11.0733 14.3755 11.7 13.878 12.18C13.3805 12.6333 12.7521 12.86 11.9928 12.86C11.0764 12.86 10.3171 12.5533 9.71484 11.94C9.13881 11.3266 8.85079 10.4467 8.85079 9.29999C8.85079 8.07332 9.19117 6.87332 9.87194 5.69999C10.5789 4.49999 11.5608 3.55332 12.8176 2.85999L13.7209 4.25999C13.0401 4.73999 12.4903 5.27332 12.0713 5.85999C11.6786 6.44666 11.4168 7.12666 11.2858 7.89999C11.5215 7.79332 11.7964 7.73999 12.1106 7.73999C12.8437 7.73999 13.446 7.97999 13.9173 8.45999C14.3886 8.93999 14.6242 9.55333 14.6242 10.3Z"
+              fill="currentColor" />
+          </svg>
+          
+          <div class="relative z-10">
+            <p><em>
+                Language learning is something so amazing that many polyglots ancestors as colonizers and scientists
+                relied on languages to evolve their reasoning and their way of thinking, in addition to learning
+                languages to develop their brains themselves, they also learned to develop communication skills that
+                were crucial in the life of them. You should also have your reason for learning new languages and if you
+                don't have something to draw inspiration from, the important thing is to learn. Start now.
+              </em></p>
+          </div>
+        </blockquote>
       </div>
     </div>
-    
   </section>
+  
+  
 </main>
 
 
@@ -72,7 +94,6 @@
   #anim {
     animation: changeColor 4s alternate ease-in-out infinite;
   }
-  
   
   @keyframes changeColor {
     from {
